24ed87284811fd6f82d1c29c5cdf1ad6a79d08b0
CeciliaLuan <123565306+CeciliaLuan@users.noreply.github.com>
2025-01-01 23:18:22 +0000

    Update urls.py

--- assignment1/heritage_sites/urls.py
+++ assignment1/heritage_sites/urls.py
@@ -1,15 +1,19 @@
-from django.urls import path, include  # Make sure to import include
+from django.urls import path
 from . import views
+from .views import HeritageSiteListView, FavouriteListView, FavouriteDetailView
 
 urlpatterns = [
-    path('', views.login_view, name='home'),  # This is the homepage view
+    path('', views.login_view, name='home'),
     path('heritage_sites/', views.map_view, name='heritage_sites'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('update_location/', views.update_location, name='update_location'),
     path('signup/', views.signup_view, name='signup'),
     path('favourites/', views.favourites_view, name='favourites'),
     path('favourites/api/add/', views.add_to_favourites, name='add_to_favourites'),
     path('favourites/api/remove/', views.remove_favourite, name='remove_favourite'),
 
+    # API routes
+    path('api/sites/', HeritageSiteListView.as_view(), name='api_sites'),
+    path('api/favourites/', FavouriteListView.as_view(), name='api_favourites'),
+    path('api/favourites/<int:pk>/', FavouriteDetailView.as_view(), name='api_favourite_detail'),
 ]
