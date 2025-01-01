6a891826099c2a1ee9932637ce1ab8ed04580eae
CeciliaLuan <123565306+CeciliaLuan@users.noreply.github.com>
2025-01-01 22:14:49 +0000

    Update settings.py

--- assignment1/assignment1/settings.py
+++ assignment1/assignment1/settings.py
@@ -134,8 +134,18 @@ PWA_APP_ICONS = [
         'type': 'image/png',
     },
     {
-        'src': '/static/images/icons/heri_128X128.png',
-        'sizes': '128x128',
+        'src': '/static/images/icons/heri_144x144.png',
+        'sizes': '144x144',
+        'type': 'image/png',
+    },
+    {
+        'src': '/static/images/icons/heri_192x192.png',
+        'sizes': '192x192',
+        'type': 'image/png',
+    },
+        {
+        'src': '/static/images/icons/heri_512x512.png',
+        'sizes': '512x512',
         'type': 'image/png',
     },
 ]
@@ -148,7 +158,7 @@ PWA_APP_ICONS_APPLE = [
 ]
 PWA_APP_SPLASH_SCREEN = [
     {
-        'src': '/static/images/icon.png',
+        'src': '/static/images/splash-640x1136.png',
         'sizes': '640x1136',
         'type': 'image/png',
     },
